2a884d32b259d15c659ec5adae862bdfe719abb0
Wang Siqi <wangsiqi@4paradigm.com>
2023-09-25 15:07:00 +0800

    add demo code

--- frontend/src/components/DagPage.vue
+++ frontend/src/components/DagPage.vue
@@ -13,8 +13,15 @@
   
 <script>
 import axios from 'axios'
-
-import { Graph } from '@antv/x6';
+import { Graph, Shape } from '@antv/x6'
+import { Stencil } from '@antv/x6-plugin-stencil'
+import { Transform } from '@antv/x6-plugin-transform'
+import { Selection } from '@antv/x6-plugin-selection'
+import { Snapline } from '@antv/x6-plugin-snapline'
+import { Keyboard } from '@antv/x6-plugin-keyboard'
+import { Clipboard } from '@antv/x6-plugin-clipboard'
+import { History } from '@antv/x6-plugin-history'
+import insertCss from 'insert-css'
 
 
 export default {
@@ -54,20 +61,388 @@ export default {
   },
 
   mounted() {
+    this.preWork();
     this.initData();
   },
 
   methods: {
     initData() {
-
       const graph = new Graph({
-        container: document.getElementById('container'),
+        container: document.getElementById('graph-container'),
         width: 800,
         height: 600,
+        grid: true,
+        mousewheel: {
+          enabled: true,
+          zoomAtMousePosition: true,
+          modifiers: 'ctrl',
+          minScale: 0.5,
+          maxScale: 3,
+        },
+        connecting: {
+          router: 'manhattan',
+          connector: {
+            name: 'rounded',
+            args: {
+              radius: 8,
+            },
+          },
+          anchor: 'center',
+          connectionPoint: 'anchor',
+          allowBlank: false,
+          snap: {
+            radius: 20,
+          },
+          createEdge() {
+            return new Shape.Edge({
+              attrs: {
+                line: {
+                  stroke: '#A2B1C3',
+                  strokeWidth: 2,
+                  targetMarker: {
+                    name: 'block',
+                    width: 12,
+                    height: 8,
+                  },
+                },
+              },
+              zIndex: 0,
+            })
+          },
+          validateConnection({ targetMagnet }) {
+            return !!targetMagnet
+          },
+        },
+        highlighting: {
+          magnetAdsorbed: {
+            name: 'stroke',
+            args: {
+              attrs: {
+                fill: '#5F95FF',
+                stroke: '#5F95FF',
+              },
+            },
+          },
+        },
       });
+      // #endregion
+
+      // #region 使用插件
+      graph
+        .use(
+          new Transform({
+            resizing: true,
+            rotating: true,
+          }),
+        )
+        .use(
+          new Selection({
+            rubberband: true,
+            showNodeSelectionBox: true,
+          }),
+        )
+        .use(new Snapline())
+        .use(new Keyboard())
+        .use(new Clipboard())
+        .use(new History());
+      // #endregion
+
+
+      // #region 初始化 stencil
+      const stencil = new Stencil({
+        title: 'SQL转换',
+        target: graph,
+        stencilGraphWidth: 200,
+        stencilGraphHeight: 180,
+        collapsable: true,
+        groups: [
+          {
+            title: '节点',
+            name: 'group1',
+          },
+        ],
+        layoutOptions: 
+          {    
+            columns: 1,
+            columnWidth: 80,
+            rowHeight: 55,
+          },
+      });
+
+      document.getElementById('stencil').appendChild(stencil.container);
+      // #endregion
+
+      // #region 快捷键与事件
+      graph.bindKey(['meta+c', 'ctrl+c'], () => {
+        const cells = graph.getSelectedCells()
+        if (cells.length) {
+          graph.copy(cells)
+        }
+        return false
+      })
+      graph.bindKey(['meta+x', 'ctrl+x'], () => {
+        const cells = graph.getSelectedCells()
+        if (cells.length) {
+          graph.cut(cells)
+        }
+        return false
+      })
+      graph.bindKey(['meta+v', 'ctrl+v'], () => {
+        if (!graph.isClipboardEmpty()) {
+          const cells = graph.paste({ offset: 32 })
+          graph.cleanSelection()
+          graph.select(cells)
+        }
+        return false
+      })
+
+      // undo redo
+      graph.bindKey(['meta+z', 'ctrl+z'], () => {
+        if (graph.canUndo()) {
+          graph.undo()
+        }
+        return false
+      })
+      graph.bindKey(['meta+shift+z', 'ctrl+shift+z'], () => {
+        if (graph.canRedo()) {
+          graph.redo()
+        }
+        return false
+      })
+
+      // select all
+      graph.bindKey(['meta+a', 'ctrl+a'], () => {
+        const nodes = graph.getNodes()
+        if (nodes) {
+          graph.select(nodes)
+        }
+      })
+
+      // delete
+      graph.bindKey('backspace', () => {
+        const cells = graph.getSelectedCells()
+        if (cells.length) {
+          graph.removeCells(cells)
+        }
+      })
+
+      // zoom
+      graph.bindKey(['ctrl+1', 'meta+1'], () => {
+        const zoom = graph.zoom()
+        if (zoom < 1.5) {
+          graph.zoom(0.1)
+        }
+      })
+      graph.bindKey(['ctrl+2', 'meta+2'], () => {
+        const zoom = graph.zoom();
+        if (zoom > 0.5) {
+          graph.zoom(-0.1);
+        }
+      })
+
+      // 控制连接桩显示/隐藏
+      //const showPorts = (ports: NodeListOf<SVGElement>, show: boolean) => {
+      //  for (let i = 0, len = ports.length; i < len; i += 1) {
+      //    ports[i].style.visibility = show ? 'visible' : 'hidden'
+      //  }
+      //}
+      graph.on('node:mouseenter', () => {
+        const container = document.getElementById('graph-container');
+        const ports = container.querySelectorAll(
+          '.x6-port-body',
+        ); // as NodeListOf<SVGElement>;
+
+        //showPorts(ports, true)
+
+        for (let i = 0, len = ports.length; i < len; i += 1) {
+          ports[i].style.visibility = 'visible';
+        }
+      })
+      graph.on('node:mouseleave', () => {
+        const container = document.getElementById('graph-container');
+        const ports = container.querySelectorAll(
+          '.x6-port-body',
+        ); // as NodeListOf<SVGElement>;
+
+        //showPorts(ports, false)
+
+        for (let i = 0, len = ports.length; i < len; i += 1) {
+          ports[i].style.visibility = 'hidden';
+        }
+      })
+      // #endregion
+
+      // #region 初始化图形
+      const ports = {
+        groups: {
+          top: {
+            position: 'top',
+            attrs: {
+              circle: {
+                r: 4,
+                magnet: true,
+                stroke: '#5F95FF',
+                strokeWidth: 1,
+                fill: '#fff',
+                style: {
+                  visibility: 'hidden',
+                },
+              },
+            },
+          },
+          bottom: {
+            position: 'bottom',
+            attrs: {
+              circle: {
+                r: 4,
+                magnet: true,
+                stroke: '#5F95FF',
+                strokeWidth: 1,
+                fill: '#fff',
+                style: {
+                  visibility: 'hidden',
+                },
+              },
+            },
+          },
+        },
+        items: [
+          {
+            group: 'top',
+          },
+          {
+            group: 'right',
+          },
+          {
+            group: 'bottom',
+          },
+          {
+            group: 'left',
+          },
+        ],
+      };
+
+      Graph.registerNode(
+        'custom-rect',
+        {
+          inherit: 'rect',
+          width: 66,
+          height: 36,
+          attrs: {
+            body: {
+              strokeWidth: 1,
+              stroke: '#5F95FF',
+              fill: '#EFF4FF',
+            },
+            text: {
+              fontSize: 12,
+              fill: '#262626',
+            },
+          },
+          ports: {
+            ...ports,
+            items: [
+              {
+                group: 'top',
+              },
+              {
+                group: 'top',
+              },
+              {
+                group: 'bottom',
+              },
+            ],
+          },
+        },
+        true,
+      );
+
+
+      const r1 = graph.createNode({
+        shape: 'custom-rect',
+        attrs: {
+          body: {
+            rx: 6,
+            ry: 6,
+          },
+        },
+          tools: [
+          {
+            name: 'node-editor',
+            args: {
+              attrs: {
+                backgroundColor: '#EFF4FF',
+              },
+            },
+          },
+        ],
+        label: 'Node',
+      });
+
+      stencil.load([r1], 'group1');
+
 
       graph.fromJSON(this.data);
 
+    },
+    preWork() {
+      // 这里协助演示的代码，在实际项目中根据实际情况进行调整
+      const container = document.getElementById('container');
+      const stencilContainer = document.createElement('div');
+      stencilContainer.id = 'stencil';
+      const graphContainer = document.createElement('div');
+      graphContainer.id = 'graph-container';
+      container.appendChild(stencilContainer);
+      container.appendChild(graphContainer);
+
+      insertCss(`
+        #container {
+          display: flex;
+          border: 1px solid #dfe3e8;
+        }
+        #stencil {
+          width: 180px;
+          height: 100%;
+          position: relative;
+          border-right: 1px solid #dfe3e8;
+        }
+        #graph-container {
+          width: calc(100% - 180px);
+          height: 100%;
+        }
+        .x6-widget-stencil  {
+          background-color: #fff;
+        }
+        .x6-widget-stencil-title {
+          background-color: #fff;
+        }
+        .x6-widget-stencil-group-title {
+          background-color: #fff !important;
+        }
+        .x6-widget-transform {
+          margin: -1px 0 0 -1px;
+          padding: 0px;
+          border: 1px solid #239edd;
+        }
+        .x6-widget-transform > div {
+          border: 1px solid #239edd;
+        }
+        .x6-widget-transform > div:hover {
+          background-color: #3dafe4;
+        }
+        .x6-widget-transform-active-handle {
+          background-color: #3dafe4;
+        }
+        .x6-widget-transform-resize {
+          border-radius: 0;
+        }
+        .x6-widget-selection-inner {
+          border: 1px solid #239edd;
+        }
+        .x6-widget-selection-box {
+          opacity: 0;
+        }
+      `);
     },
   },
 };
