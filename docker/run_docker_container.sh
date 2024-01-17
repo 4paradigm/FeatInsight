1045d9f610c22cde970aea4737d2949685aff750
tobe <tobeg3oogle@gmail.com>
2024-01-17 11:25:13 +0800

    Update dockerfile and run script

--- docker/run_docker_container.sh
+++ docker/run_docker_container.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-docker run -it -p 8888:8888 -v /Users/tobe/code/4pd/feature-platform/docker/application.yml:/app/application.yml registry.cn-shenzhen.aliyuncs.com/tobe43/openmldb-feature-platform
+docker run -it -p 8888:8888 -v `pwd`/application.yml:/app/application.yml registry.cn-shenzhen.aliyuncs.com/tobe43/openmldb-feature-platform
