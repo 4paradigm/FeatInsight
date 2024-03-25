10f0766a35e4e359a770047248b05092e260a0e3
xuqichen <xuqichen@4paradigm.com>
2024-03-25 15:06:30 +0800

    allow options request to pass check

--- src/main/java/com/_4paradigm/openmldb/featureplatform/interceptor/SqlExecutorInterceptor.java
+++ src/main/java/com/_4paradigm/openmldb/featureplatform/interceptor/SqlExecutorInterceptor.java
@@ -4,6 +4,7 @@ import com._4paradigm.openmldb.featureplatform.dao.model.ThreadLocalSqlExecutor;
 import com._4paradigm.openmldb.featureplatform.utils.SqlExecutorPoolManager;
 import com._4paradigm.openmldb.sdk.impl.SqlClusterExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -18,6 +19,11 @@ public class SqlExecutorInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // do not need to check OPTIONS request
+        if(request.getMethod().equals(HttpMethod.OPTIONS.name())) {
+            return true;
+        }
+
         String URI = request.getRequestURI();
         if(URI.equals("/api/login")) {
             return true;
