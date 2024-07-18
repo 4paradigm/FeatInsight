ad466f78e424fb853b4fa9c5d042366e61be3fa7
shanbinbin <shanbinbin@4paradigm.com>
2024-07-18 19:15:00 +0800

    feat: OPRFM-7109

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/openmldb-featinsight',
   build: {
     outDir: '../src/main/resources/static/'
   },
@@ -13,5 +14,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    host: '0.0.0.0', // 或者 'localhost' 或者你想要绑定的其他可用的IP
+    port: 5173,
+  },
 })
