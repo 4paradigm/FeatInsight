9d6445a924f2a6bd9cacb9fddc70ba7cf67eca03
Yuan Haitao <yuanhaitao@4paradigm.com>
2024-07-23 19:47:18 +0800

    add helm-chart config

--- src/main/java/com/_4paradigm/openmldb/featureplatform/client/ProphetTelamonClient.java
+++ src/main/java/com/_4paradigm/openmldb/featureplatform/client/ProphetTelamonClient.java
@@ -1,5 +1,6 @@
 package com._4paradigm.openmldb.featureplatform.client;
 
+import com._4paradigm.openmldb.featureplatform.controller.FesqlPredictorController;
 import com._4paradigm.openmldb.featureplatform.dao.model.FesqlTableColumn;
 import com._4paradigm.openmldb.featureplatform.dao.model.SimpleTableInfo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -12,50 +13,79 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ProphetTelamonClient {
+    private static final Logger logger = LoggerFactory.getLogger(ProphetTelamonClient.class);
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    private final CloseableHttpClient httpClient;
     private final String apiEndpoint;
     private final Map<String, String> headers;
 
     public ProphetTelamonClient(String apiEndpoint, Map<String, String> headers) {
-        this.httpClient = HttpClients.createDefault();
         this.apiEndpoint = apiEndpoint;
         this.headers = headers;
     }
 
-    /**
-     * Get Prophet table-schema.
-     *
-     * @return
-     * @throws Exception
-     */
     public List<FesqlTableColumn> getTableSchema(String tablePrn) throws Exception {
-        String endpoint = String.format("%s/v1/tables?prn=%s", this.apiEndpoint, tablePrn);
-        HttpGet request = new HttpGet(endpoint);
-        for (Map.Entry<String, String> entry : headers.entrySet()) {
-            request.setHeader(entry.getKey(), entry.getValue());
+        long startTime = System.currentTimeMillis();
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            String url = String.format("%s/telamon/v1/tables?prn=%s", this.apiEndpoint, tablePrn);
+            HttpGet httpGet = new HttpGet(url);
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                httpGet.setHeader(entry.getKey(), entry.getValue());
+            }
+            HttpResponse response = httpClient.execute(httpGet);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 200) {
+                throw new RuntimeException(String.format("Fail get table prn: %s, http status code: %s", tablePrn, statusCode));
+            }
+            HttpEntity entity = response.getEntity();
+            String responseBody = EntityUtils.toString(entity);
+            QueryTableResponse tableResponse = objectMapper.readValue(responseBody, QueryTableResponse.class);
+            if (!"0".equals(tableResponse.getStatus()) || tableResponse.getData().getList().isEmpty()) {
+                throw new Exception(String.format("Fail get table prn: %s, responseBody: %s", tablePrn, response));
+            }
+            return tableResponse.getData().getList().get(0).getSchema();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            long endTime = System.currentTimeMillis();
+            logger.info("get table times: {} ms, prn: {}", endTime - startTime, tablePrn);
         }
-        HttpResponse response = httpClient.execute(request);
-        HttpEntity entity = response.getEntity();
-        String responseBody = EntityUtils.toString(entity);
-
-        if (response.getStatusLine().getStatusCode() != 200) {
-            throw new IOException("Fail to request server and get error: " + responseBody);
-        }
-        QueryTableResponse tableResponse = objectMapper.readValue(responseBody, QueryTableResponse.class);
-        if (!"0".equals(tableResponse.getStatus()) || tableResponse.getData().getList().isEmpty()) {
-            throw new Exception(String.format("Fail get table prn: %s, responseBody: %s", tablePrn, response));
-        }
-        return tableResponse.getData().getList().get(0).getSchema();
     }
+
+//    /**
+//     * Get Prophet table-schema.
+//     *
+//     * @return
+//     * @throws Exception
+//     */
+//    public List<FesqlTableColumn> getTableSchema(String tablePrn) throws Exception {
+//        String endpoint = String.format("%s/v1/tables?prn=%s", this.apiEndpoint, tablePrn);
+//        HttpGet request = new HttpGet(endpoint);
+//        for (Map.Entry<String, String> entry : headers.entrySet()) {
+//            request.setHeader(entry.getKey(), entry.getValue());
+//        }
+//        HttpResponse response = httpClient.execute(request);
+//        HttpEntity entity = response.getEntity();
+//        String responseBody = EntityUtils.toString(entity);
+//
+//        if (response.getStatusLine().getStatusCode() != 200) {
+//            throw new IOException("Fail to request server and get error: " + responseBody);
+//        }
+//        QueryTableResponse tableResponse = objectMapper.readValue(responseBody, QueryTableResponse.class);
+//        if (!"0".equals(tableResponse.getStatus()) || tableResponse.getData().getList().isEmpty()) {
+//            throw new Exception(String.format("Fail get table prn: %s, responseBody: %s", tablePrn, response));
+//        }
+//        return tableResponse.getData().getList().get(0).getSchema();
+//    }
 
     @Data
     @JsonIgnoreProperties(ignoreUnknown = true)
@@ -78,9 +108,5 @@ public class ProphetTelamonClient {
         private String prn;
         private String name;
         private List<FesqlTableColumn> schema;
-    }
-
-    public void close() throws IOException {
-        this.httpClient.close();
     }
 }
